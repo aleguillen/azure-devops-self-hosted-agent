9cfcd9979418a7530ca68238b8c2fc12040349bd
Alejandra Guillen <algut@microsoft.com>
2020-07-22 08:19:10 -0500

    Adding Role Assigment to Key Vault

--- infra/rbac.tf
+++ infra/rbac.tf
@@ -6,3 +6,11 @@ resource "azurerm_role_assignment" "ado" {
   role_definition_name = "Contributor"
   principal_id         = var.ado_vmss_enabled ? azurerm_linux_virtual_machine_scale_set.ado.0.identity.0.principal_id : azurerm_linux_virtual_machine.ado.0.identity.0.principal_id
 }
+
+
+# CREATE: Role Assignment to Key Vault using Current Client Configuration
+resource "azurerm_role_assignment" "kv" {
+  scope                = azurerm_key_vault.ado.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
