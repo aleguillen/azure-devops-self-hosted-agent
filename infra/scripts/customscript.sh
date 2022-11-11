6a7bd0d480c8dcb13482ad264e06864ef5783c0f
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-11-10 21:23:09 -0600

    Update customscript.sh

--- infra/scripts/customscript.sh
+++ infra/scripts/customscript.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 echo "Updating packages ..."
 
-sudo apt-get update
-apt upgrade -y
-sudo apt install curl wget zip unzip apt-transport-https ca-certificates gnupg-agent software-properties-common lsb-release gnupg -y
-
-apt install curl -y
-apt install wget -y
-apt install unzip -y
+while sudo fuser /var/lib/dpkg/lock /var/lib/apt/lists/lock /var/cache/apt/archives/lock >/dev/null 2>&1; do echo 'Waiting for release of dpkg/apt locks'; sleep 15; done; sudo apt-get install -y curl wget unzip apt-transport-https ca-certificates gnupg-agent software-properties-common lsb-release gnupg;
+while sudo fuser /var/lib/dpkg/lock /var/lib/apt/lists/lock /var/cache/apt/archives/lock >/dev/null 2>&1; do echo 'Waiting for release of dpkg/apt locks'; sleep 15; done; sudo apt-get update
 
 echo "[$(date +%F_%T)] Installing Azure CLI"
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+while sudo fuser /var/lib/dpkg/lock /var/lib/apt/lists/lock /var/cache/apt/archives/lock >/dev/null 2>&1; do echo 'Waiting for release of dpkg/apt locks'; sleep 15; done; curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #sudo apt install --only-upgrade -y azure-cli
 
 echo "[$(date +%F_%T)] Installing Terraform"
 sudo wget https://releases.hashicorp.com/terraform/1.0.4/terraform_1.0.4_linux_amd64.zip
+apt install unzip -y
 sudo unzip terraform*.zip
 sudo mv terraform /usr/local/bin
 
