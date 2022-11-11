69b4c8d1a9f55dce9f793ccf8e0a53743e29b8e1
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-11-10 19:21:25 -0600

    Update customscript.sh

--- infra/scripts/customscript.sh
+++ infra/scripts/customscript.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 apt upgrade -y
 sudo apt install curl wget zip unzip apt-transport-https ca-certificates gnupg-agent software-properties-common lsb-release gnupg -y
 
+apt install curl -y
+apt install wget -y
+apt install unzip -y
+
 echo "[$(date +%F_%T)] Installing Azure CLI"
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt install --only-upgrade -y azure-cli
