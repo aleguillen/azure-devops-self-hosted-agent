5426b127adcf431ff24f7ef116c05b0416758420
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-11-10 18:28:41 -0600

    Create customscript.sh

---
+++ infra/scripts/customscript.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Updating packages ..."
+
+sudo apt-get update
+apt upgrade -y
+sudo apt install curl wget zip unzip apt-transport-https ca-certificates gnupg-agent software-properties-common lsb-release gnupg -y
+
+echo "[$(date +%F_%T)] Installing Azure CLI"
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt install --only-upgrade -y azure-cli
+
+echo "[$(date +%F_%T)] Installing Terraform"
+sudo wget https://releases.hashicorp.com/terraform/1.0.4/terraform_1.0.4_linux_amd64.zip
+sudo unzip terraform*.zip
+sudo mv terraform /usr/local/bin
+
